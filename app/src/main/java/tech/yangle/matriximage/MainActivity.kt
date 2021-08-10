f127ee3caf344acff4b8de3baf8659b55670e1f2
yangle <mr.yang0229@foxmail.com>
2021-08-10 16:55:19 +0800

    项目初始创建

---
+++ app/src/main/java/tech/yangle/matriximage/MainActivity.kt
@@ -0,0 +1,18 @@
+package tech.yangle.matriximage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * 首页
+ * <p>
+ * Created by yangle on 2021/8/10.
+ * Website：http://www.yangle.tech
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
